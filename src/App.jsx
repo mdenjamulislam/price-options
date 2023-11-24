8be8acc7106af809d03437846749f702ef36d6fc
Enjamul Islam <mdenjamulislam527@gmail.com>
2023-11-24 18:11:14 +0600

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import DaisyNav from './components/Daisynav/Daisynav'
+
+function App() {
+
+  return (
+    <>
+      <div className='container mx-auto' data-theme="dark">
+        <DaisyNav></DaisyNav>
+      </div>
+      
+      
+    </>
+  )
+}
+
+export default App
